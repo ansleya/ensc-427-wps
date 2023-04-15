e510fcdb21cad6297913fa6485b611d4de4f5ed5
ansleya <55563396+ansleya@users.noreply.github.com>
2023-04-14 17:38:47 -0700

    updated comments for submission
    Updated my comments to submit the project. also updated the "minion" variable names to "beacon"

--- main.cc
+++ main.cc
@@ -18,7 +18,6 @@
 #include "ns3/internet-module.h"
 #include "ns3/mobility-module.h"
 #include "ns3/network-module.h"
-//#include "ns3/point-to-point-module.h"
 #include "ns3/ssid.h"
 #include "ns3/mesh-helper.h"
 #include "ns3/mesh-module.h"
@@ -30,35 +29,44 @@
 #include <fstream>
 #include <string>
 
-// comment out for testing
-#define MALL_CONFIG
-// Default Network Topology
+// comment out for testing with old 25m x 25m configuration
+#define MALL_CONFIG // for the 15m x 150m mall environment
+/* Ideal Network Topology
 //
 //   Wifi 10.1.3.0
-//                 AP
-//  *    *    *    *
-//  |    |    |    |    10.1.1.0
-// n5   n6   n7   n0 -------------- n1   n2   n3   n4
-//                   point-to-point  |    |    |    |
-//                                   ================
-//                                     LAN 10.1.2.0
+//  AP              AP
+//  *               * 
+//  | \           / |  
+// n1  \         /  n2  
+        \  STA  /
+            *
+            |       
+            x           x = {n0,n5,n7...}
+          /     \
+//  AP   /       \  AP
+//  *               * 
+//  |               |  
+// n3               n4   
+*/
 
+/* Real Network Topology
+//
+//   Wifi 10.1.3.0
+//  AP             
+//  *               
+//  | \           
+// n1  \         
+        \  STA  
+            *
+            |           Repeat simulation for AP -> n1,n2,n3,n4
+            x          x = {n0,n5,n7...}    
+
+*/
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("WPS");
 
-int numPacketDrop = 0;
-static void
-RxDrop(Ptr<OutputStreamWrapper> stream, std::string context, Ptr<const Packet> p)
-{
-    double timeNow = Simulator::Now().GetSeconds();
-    numPacketDrop++;
-    *stream->GetStream() << std::to_string(timeNow) << "\t"
-                         << context << "\t"
-                         << std::to_string(numPacketDrop)
-                         << std::endl;
-}
-
+// Callback function that outputs the current position of a node when it reaches its next destination
 void
 CourseChange (Ptr<OutputStreamWrapper> stream, std::string context, Ptr<const MobilityModel> model)
 {
@@ -67,8 +75,10 @@ CourseChange (Ptr<OutputStreamWrapper> stream, std::string context, Ptr<const Mo
   *stream->GetStream() << std::to_string(timeNow) << "\t" << position.x << "\t" << position.y << std::endl;
 
 }
-int choiceServerNum = 0;
 
+int choiceServerNum = 0; // The beacon in which the clients will be transmitting to for this simulation
+
+// Callback function that outputs the RTT for each client ping application
 static void PingRtt (Ptr<OutputStreamWrapper> stream,std::string context, Time rtt)
 {
   double timeNow = Simulator::Now().GetSeconds();
@@ -76,7 +86,7 @@ static void PingRtt (Ptr<OutputStreamWrapper> stream,std::string context, Time r
   int clientNodeNum = -1;
   int serverNodeNum = -1;
   int pos = 0;
-  for(int i = 0; i < 2; i++)
+  for(int i = 0; i < 2; i++) //this section is for extracting the client and beacon number
   {
       int number;
       std::size_t const n = context.find_first_of("0123456789",pos);
@@ -100,7 +110,6 @@ static void PingRtt (Ptr<OutputStreamWrapper> stream,std::string context, Time r
       }
 
   }
-    //std::cout << context << "=" << rtt.GetNanoSeconds () << " ns" << std::endl;
     *stream->GetStream() << std::to_string(timeNow) << "\t" << clientNodeNum << "\t"
                          << choiceServerNum << "\t" << rtt.GetNanoSeconds () << std::endl;
 }
@@ -108,63 +117,56 @@ static void PingRtt (Ptr<OutputStreamWrapper> stream,std::string context, Time r
 int
 main(int argc, char* argv[])
 {
-    //bool verbose = true;
-    //uint32_t nCsma = 3;
+
     uint32_t nWifiClient = 1; // number of mall users
-    std::string traceFile = "scenario1.ns_movements"; // mobility model traces
-    //bool tracing = false;
 
-
+    // two command line arguements for number of clients in the network and which beacon to set position for
     CommandLine cmd(__FILE__);
     cmd.AddValue("nWifiClient", "Number of wifi STA devices (# of people in the mall)", nWifiClient);
     cmd.AddValue("choiceServerNum", "select the server AP node to have the clients ping", choiceServerNum);
-    //cmd.AddValue("traceFile","NS2 movement trace file from BonnMotion",traceFile);
-    //cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
-    //cmd.AddValue("tracing", "Enable pcap tracing", tracing);
 
     cmd.Parse(argc, argv);
 
-    if(traceFile.empty()) {
-        std::cout << "Usage of " << argv[0] << ":\n" << "./waf --run \"script --traceFile=/path/to/tracefile\"\n";
-    }
-
-    std::string logFile = "router" + std::to_string(choiceServerNum) + ".txt";
-
+    std::string logFile = "router" + std::to_string(choiceServerNum) + ".txt"; // for outputting RTT data
 
     LogComponentEnable("WPS", LOG_LEVEL_INFO);
 
+    // Initiallize n amount of clients nodes and one beacon node. Store in their own containters
     NodeContainer wifiStaNodes;
     wifiStaNodes.Create(nWifiClient);
-    NodeContainer wifiApNodeMinions;
-    wifiApNodeMinions.Create(1); // 4 RTT values
-
-
-    //Config::SetDefault("ns3::WifiPhy::ChannelNumber",UintegerValue(4))
+    NodeContainer wifiApNodeBeacons;
+    wifiApNodeBeacons.Create(1); 
+    
+    // Configure a shared Wi-fi channel and install it onto the PHY
     YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
     YansWifiPhyHelper phy;
     phy.SetChannel(channel.Create());
-    //YansWifiPhyHelper phy = YansWifiPhyHelper::Default();
-    //phy.Set("ChannelNumber",UintegerValue(4));
-
+  
+    // Configure the Wifi Mac
     WifiMacHelper mac;
     Ssid ssid = Ssid("ns-3-ssid");
+
+    // Create Device containers for STA and AP nodes
     NetDeviceContainer staDevices;
-    NetDeviceContainer apDevicesMinions;
+    NetDeviceContainer apDevicesBeacons;
 
-    WifiHelper wifi; //Wifi 6, 5GHz, constant propagation delay, Friis loss model
+    WifiHelper wifi; //Wifi 6, 5GHz, constant propagation delay, logdistance loss model - default settings
 
-
+    // Install the PHY and MAC configurations for both the clients and beacon - Beacon is AP since it's the main server
     mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid), "ActiveProbing", BooleanValue(false));
     staDevices = wifi.Install(phy, mac, wifiStaNodes);
 
+    // Clients are not probing, Beacon is set to active probing
     mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
-    apDevicesMinions = wifi.Install(phy, mac, wifiApNodeMinions);
+    apDevicesBeacons = wifi.Install(phy, mac, wifiApNodeBeacons);
 
+    // Install mobility behaviour onto the nodes and starting positions
     MobilityHelper mobilityAP;
     Ptr<ListPositionAllocator> positionAllocAP = CreateObject <ListPositionAllocator>();
 
     double xposRout,yposRout;
 #ifdef MALL_CONFIG
+    // set the beacon position to it's corresponding spot, depending on which simulation you're running
     if(choiceServerNum % 2 == 0)
     {
         xposRout = 12.5+25*choiceServerNum/2;
@@ -178,7 +180,7 @@ main(int argc, char* argv[])
         positionAllocAP->Add(Vector(xposRout, yposRout, 0));
     }
 #else
-    switch(choiceServerNum)
+    switch(choiceServerNum) // old, set positions for 4 beacons in a 25 m x 25m room at the corners
     {
         case 0:
             positionAllocAP ->Add(Vector(12.5, 12.5, 0)); // node0
@@ -191,36 +193,40 @@ main(int argc, char* argv[])
     }
 
 #endif
-
+    // set the clients to random positions in the mall
     mobilityAP.SetPositionAllocator(positionAllocAP);
-    mobilityAP.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-    mobilityAP.Install(wifiApNodeMinions);
+    mobilityAP.SetMobilityModel("ns3::ConstantPositionMobilityModel"); // AP beacons are stationary
+    mobilityAP.Install(wifiApNodeBeacons);
 
     MobilityHelper mobilitySTA;
     Ptr<ListPositionAllocator> positionAllocSTA = CreateObject <ListPositionAllocator>();
 
 #ifdef MALL_CONFIG
-    srand(125); //set a seed for consistency
+    srand(125); //set a seed for consistency across trials
+
     for(uint32_t i = 0; i < nWifiClient; i++)
     {
         float xstart =150*((float) rand()/RAND_MAX);
         float ystart = 15*((float) rand()/RAND_MAX);
         positionAllocSTA ->Add(Vector(xstart, ystart, 0));
     }
+    //set the type of mobility to RandomWalk2dMobility
     mobilitySTA.SetPositionAllocator(positionAllocSTA);
     for(int i = 0; i < nWifiClient;i++)
     {
+        // speed values based on journal on shopping mall behaviour, direction is 360 degree range
+        // stream values ensure that the same path is taken across different simulations
         std::string randSpeed = "ns3::UniformRandomVariable[Min=1.15|Max=1.65|Stream=" + std::to_string(i+1) + "]";
         std::string randDirection = "ns3::UniformRandomVariable[Min=0.0|Max=6.283184|Stream=" + std::to_string(i+1) + "]";
 
         mobilitySTA.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
                                      "Speed", StringValue(randSpeed),
                                      "Direction", StringValue(randDirection),
-                                     //"Pause", StringValue("ns3::ConstantRandomVariable[Constant=2]"),
-                                     "Bounds", StringValue("0|150|0|15"));
+                                     "Bounds", StringValue("0|150|0|15")); // mall boundary
         mobilitySTA.Install(wifiStaNodes.Get(i));
     }
 #else
+    // old testing
     srand(100); //set a seed for consistency
     for(uint32_t i = 0; i < nWifiClient; i++)
     {
@@ -236,68 +242,59 @@ main(int argc, char* argv[])
     mobilitySTA.Install(wifiStaNodes);
 #endif
 
+    // Installation allows the nodes to use internet layer protocols (TCP, UDP, ARP, IPv4, etc.)
     InternetStackHelper stack;
-   // stack.Install(csmaNodes);
-    stack.Install(wifiApNodeMinions);
+    stack.Install(wifiApNodeBeacons);
     stack.Install(wifiStaNodes);
 
+    // Set the IP address of all nodes to be in the same subnet
     Ipv4AddressHelper address;
 
     address.SetBase("10.1.3.0", "255.255.255.0");
-    Ipv4InterfaceContainer apminionsInterfaces =  address.Assign(apDevicesMinions);
+    Ipv4InterfaceContainer apbeaconsInterfaces =  address.Assign(apDevicesBeacons);
     Ipv4InterfaceContainer staInterfaces =  address.Assign(staDevices);
 
     ApplicationContainer clientApps[nWifiClient];
-
+    // install the V4Ping application to the client nodes - the beacon has no application but still handles the echo requests from the stack helper
     uint32_t simTime = 60;
-    //for(int i = 0;i < 4; i++)
-    //{
-    V4PingHelper pingServer(apminionsInterfaces.GetAddress (0));
-    // install 4 ping applications to he phone client
-    pingServer.SetAttribute("Interval",TimeValue(Seconds(1.)));
+    V4PingHelper pingServer(apbeaconsInterfaces.GetAddress (0)); // the Beacon IP address
+    pingServer.SetAttribute("Interval",TimeValue(Seconds(1.))); // send a new packet every second
     //pingServer.SetAttribute("Verbose",BooleanValue(true));
     srand(100);
-    for(int i = 0; i<nWifiClient;i++)
+    for(int i = 0; i<nWifiClient;i++) // add jitter to the start times for the ping applications
     {
         clientApps[i].Add(pingServer.Install(wifiStaNodes.Get(i)));
         clientApps[i].Start(Seconds(1.0+.02*i));
         clientApps[i].Stop(Seconds(simTime));
     }
-    //}
-    //"/NodeList/[i]/ApplicationList/[i]/$ns3::V4Ping"
+
+    // sets the routing path for each node - not very complex since it's only a single hop from client to beacon
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-    
+    // Set the simulation stop time
     Simulator::Stop(Seconds(simTime));
 
-
-
-    // open log file for output
+    // open log files for output
     AsciiTraceHelper asciiTraceHelper;
     Ptr<OutputStreamWrapper> stream = asciiTraceHelper.CreateFileStream(logFile);
-    //Ptr<OutputStreamWrapper> stream = asciiTraceHelper.CreateFileStream("test.txt");
     Ptr<OutputStreamWrapper> streamMobility = asciiTraceHelper.CreateFileStream("client0-pos.txt");
 
-    std::string logFileDrops = "droppedPackets" + std::to_string(choiceServerNum);
-    Ptr<OutputStreamWrapper> streamPhyDrop = asciiTraceHelper.CreateFileStream(logFileDrops);
-
-    // Configure callback for logging
+    // Configure callback for logging - trace sources
     Config::Connect("/NodeList/0/$ns3::MobilityModel/CourseChange",
                     MakeBoundCallback(&CourseChange, streamMobility));
     Config::Connect ("/NodeList/*/ApplicationList/*/$ns3::V4Ping/Rtt",
                      MakeBoundCallback (&PingRtt,stream));
-    //Config::Connect ("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Mac/MacPromiscRx",
-    //                 MakeBoundCallback(&RxDrop, streamPhyDrop));
 
-   // std::string packetDropContext = "/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/$ns3::YansWifiPhy/PhyRxDrop";
-    //Config::Connect(packetDropContext,
-     //               MakeBoundCallback(&RxDrop, streamPhyDrop));
+    // Write a header to the log files
     *stream->GetStream() << std::to_string(nWifiClient) << "\t" << std::to_string(xposRout)
                          << "\t" << std::to_string(yposRout)
                          << "\t" << std::to_string((simTime - 1)*nWifiClient) <<std::endl;
 
-    *streamMobility->GetStream() << std::to_string(nWifiClient) << "\t0" << "\t0" <<"\t" << std::to_string((simTime - 1)*nWifiClient) << std::endl;
-    *streamPhyDrop->GetStream() << std::to_string(nWifiClient) << std::endl;
+    *streamMobility->GetStream() << std::to_string(nWifiClient) 
+                                 << "\t0" << "\t0" <<"\t" 
+                                 << std::to_string((simTime - 1)*nWifiClient) << std::endl;
+    
+    // Run the simulation
     Simulator::Run();
     Simulator::Destroy();
     std::cout << "Done Simulation with nWifiClient="
